98bc558cba4b85accb928fef26a3094f835807f7
webkaran18 <webdevkaran18@gmail.com>
2026-06-15 17:35:23 +0530

    .

--- video86/main.js
+++ video86/main.js
@@ -1,17 +1,28 @@
 // Further Reading: https://nodejs.org/en/learn/getting-started/introduction-to-nodejs
-// const http = require('node:http');
+// const http = require('node:http'); //default common js
 
-import http from "http"
+// import http from "http"  //module
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+
+// import {a} from "./mymodule.js"
+// console.log(a)
+
+
+
+import obj from "./mymodule.js"
+console.log(obj)
+
